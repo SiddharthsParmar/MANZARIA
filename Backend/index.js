e1137d6137669ad7f6b9798dc503d26e20010942
Siddharth <siddharthakk3704@gmail.com>
2024-11-02 18:12:07 +0530

    Second Commit

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+// this file is main server 
+import express from 'express';
+import dotenv from 'dotenv'
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.port;
+const mode = process.env.mode;
+
+app.get("/",(req,res)=>{
+
+    res.send(
+       " <h1>Welcome my friend</h1>"
+    )
+
+});
+app.listen(port , ()=>{
+    console.log("server is running on : " + mode+ "mode" +" on port:  "+ port);
+    
+})
