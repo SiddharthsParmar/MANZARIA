f5efabd64669bccbcec48daac1f46a00704772b3
Siddharth <siddharthakk3704@gmail.com>
2024-11-05 13:55:17 +0530

    Frontend Intiated

---
+++ Client/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import Layout from './components/Layout/Layout.jsx'
+
+function App() {
+  return (
+  
+  <>
+  <Layout>
+
+  <h1>hello</h1>
+  </Layout>
+ 
+
+    </>
+  )
+}
+
+export default App
